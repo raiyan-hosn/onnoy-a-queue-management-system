ba41041aa6d519b651b5555193b32f690753226a
tawhidib <tawhidib@gmail.com>
2021-08-15 11:29:26 +0600

    invited email problem

--- js/invidetedEmailAdding.js
+++ js/invidetedEmailAdding.js
@@ -1,3 +1,92 @@
+// variable and selectors
+
+invitedEmails = [];
+
+// invited email adder button all workds
+const invitedEmailInput = document.getElementById("invitedEmailInput");
+const invitedEmailButton = document.getElementById("invitedEmailButton");
+const invitedEmailType = document.getElementById("invitedEmailType");
+
+invitedEmailButton.classList.add("button");
+
+invitedEmailButton.addEventListener("click", function () {
+  invitedEmails.push([invitedEmailInput.value, invitedEmailType.value]);
+
+  invitedEmailInput.value = "";
+  invitedEmailType.value = "Desk";
+});
+
+//invited email adder button works ends here...
+
+// invited email list all works start from here...
+
+const inviteEmailArea = document.querySelector("#invited-eamil-area");
+
+// logical part
+if (invitedEmails.length == 0) {
+  const noOneInvited = document.createElement("div");
+  noOneInvited.innerText = "Currently no one is invited to this queue";
+  noOneInvited.classList.add("no-one-invited");
+  inviteEmailArea.appendChild(noOneInvited);
+} else {
+  const createList = document.createElement("ul");
+
+  for (let i = 0; i < invitedEmails.length; i++) {
+    const listItem = document.createElement("li");
+    const listItemDiv = document.createElement("div");
+    const listItemTrashDiv = document.createElement("div");
+    // adding bootstrap class on listItemDiv
+    listItem.classList.add("d-flex");
+    listItem.classList.add("justify-content-center");
+    // listItem.classList.add("align-items-center");
+    listItem.classList.add("mt-4");
+    // adding bootstrap class and custom class on listItemDiv
+    listItemDiv.classList.add("d-flex");
+    listItemDiv.classList.add("justify-content-center");
+    listItemDiv.classList.add("align-items-center");
+    listItemDiv.classList.add("invitedEmail-item-info");
+
+    const listItemImageDiv = document.createElement("div");
+    const listItemInfoDiv = document.createElement("div");
+
+    const listItemImage = document.createElement("img");
+    const listItemInfoEmail = document.createElement("h6");
+    const listItemInfoType = document.createElement("h6");
+    const listItemTrashIcon = document.createElement("i");
+
+    // image link will give by rayhan
+    // setting image for now
+    listItemImage.src = `${i + 1}.jpg`;
+    listItemImageDiv.appendChild(listItemImage);
+    listItemImageDiv.classList.add("invitedEmail-item-info-img");
+    listItemDiv.appendChild(listItemImageDiv);
+
+    // Info collecting
+    listItemInfoEmail.innerText = invitedEmails[i][0];
+    listItemInfoType.innerText = invitedEmails[i][1];
+    listItemInfoDiv.appendChild(listItemInfoEmail);
+    listItemInfoDiv.appendChild(listItemInfoType);
+    listItemDiv.appendChild(listItemInfoDiv);
+
+    // trash button adding ...
+    listItemTrashIcon.classList.add("fas");
+    listItemTrashIcon.classList.add("fa-times");
+    listItemTrashDiv.classList.add("invitedEmail-item-trash");
+    listItemTrashDiv.classList.add("rounded-circle");
+    listItemTrashDiv.appendChild(listItemTrashIcon);
+
+    listItem.appendChild(listItemDiv);
+    listItem.appendChild(listItemTrashDiv);
+    createList.appendChild(listItem);
+  }
+
+  inviteEmailArea.appendChild(createList);
+}
+
+// fuctions and event handler
+
+/* previous works
+
 // Selectors
 
 const invitedEmailInput = document.querySelector("#invitedEmailInput");
@@ -58,3 +147,5 @@ function deleteCheck(e) {
     });
   }
 }
+
+*/

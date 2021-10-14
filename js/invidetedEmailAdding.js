a5574f4a41fd78a072f6e36108e131aa90e02e64
raiyan-hosn <raiyan.hosn@gmail.com>
2021-10-14 14:28:45 +0600

    photo fetch on new design

--- js/invidetedEmailAdding.js
+++ js/invidetedEmailAdding.js
@@ -15,6 +15,7 @@ invitedEmailButton.addEventListener("click", function () {
   if (invitedEmailInput.value !== "") {
     invitedEmails.push([invitedEmailInput.value, invitedEmailType.value]);
   }
+  
   invitedEmailInput.value = "";
   invitedEmailType.value = "Desk";
   createList.innerHTML = "";
@@ -28,40 +29,54 @@ invitedEmailButton.addEventListener("click", function () {
 // new addPerson fuction
 
 const newAddPerson = (email, type) => {
-  const listItem = document.createElement("li");
-  listItem.classList.add("row");
-  listItem.classList.add("justify-content-center");
-  listItem.classList.add("align-items-center");
-  listItem.classList.add("mx-auto");
-  listItem.classList.add("my-2");
+  
+  usersRef.child(filterPath(email))
+          .once("value",function(snapshot){
+            if(snapshot.val()==null){
 
-  listItem.innerHTML = `
-    <div class="row col m-0 invitedEmail-item-info d-flex justify-content-center align-items-center">
-      <div class="invitedEmail-item-info-img col-3">
-        <img src=""/>
-      </div>
-      <div class="col-6">
-        <h5>invited person name</h5>
-        <h6>${email}</h6>
-      </div>
-      <div class="col-3">
-        <h5>${type}</h5>
-      </div>
-    </div>
+            }
+            else{
+              let photoURL = snapshot.val().photoURL;
+              let name= snapshot.val().name;
+              const listItem = document.createElement("li");
+              listItem.classList.add("row");
+              listItem.classList.add("justify-content-center");
+              listItem.classList.add("align-items-center");
+              listItem.classList.add("mx-auto");
+              listItem.classList.add("my-2");
+            
+              listItem.innerHTML = `
+                <div class="row col m-0 invitedEmail-item-info d-flex justify-content-center align-items-center">
+                  <div class="invitedEmail-item-info-img col-3">
+                    <img src="${photoURL}"/>
+                  </div>
+                  <div class="col-6">
+                    <h5>${name}</h5>
+                    <h6>${email}</h6>
+                  </div>
+                  <div class="col-3">
+                    <h5>${type}</h5>
+                  </div>
+                </div>
+            
+                // trash button 
+            
+                <div class="trash invitedEmail-item-trash rounded-circle col-3">
+                  <i class="trash fas fa-times"></i>
+                </div>
+              `;
+            
+              listItem.addEventListener("click", function (event) {
+                if (event.target.classList[0] === "trash") {
+                  const targatedLi = event.target.parentElement;
+                  targatedLi.remove();
+                }
+              });
+            
+              createList.appendChild(listItem);
 
-    // trash button 
+            }
+          });
 
-    <div class="trash invitedEmail-item-trash rounded-circle col-3">
-      <i class="trash fas fa-times"></i>
-    </div>
-  `;
-
-  listItem.addEventListener("click", function (event) {
-    if (event.target.classList[0] === "trash") {
-      const targatedLi = event.target.parentElement;
-      targatedLi.remove();
-    }
-  });
-
-  createList.appendChild(listItem);
+ 
 };

84c39537178e77175aa8520abe33341c1c2e43ed
tawhidib <tawhidib@gmail.com>
2021-12-26 20:03:27 +0600

    error

--- js/previous.js
+++ js/previous.js
@@ -1,55 +1,134 @@
-
-
 const prevContainer = document.getElementById("prev-container");
 
-
-
 firebase.auth().onAuthStateChanged(function (user) {
-	if (user) {
-			// User is signed in.
-			let email=user.email;
-			email=filterPath(email);
-			usersRef.child(email+"/previousList").on("value",function(snapshot){
-				if(snapshot.val()==null){
-					//not found
-					
-				}else{
-					let qidList=Object.keys(snapshot.val());
-					let prevQueue = "";
-					for(let i=0;i<Object.keys(snapshot.val()).length;i++){
-						let qid=qidList[i];
-						let access=snapshot.val()[qid];
-						queuesRef.child(qid).on("value",function(snapshot){
-							if(snapshot.val()==null){
-								//queue not found
-							}
-							else{
-								let title= snapshot.val().tittle;
-								let time=snapshot.val().time;
-								let owner=snapshot.val().owner;
-								
-								prevQueue =
-										
-										`<div class="col-md-4 col-sm-6">
+    if (user) {
+        // User is signed in.
+        let email = user.email;
+        email = filterPath(email);
+        usersRef
+            .child(email + "/previousList")
+            .on("value", function (snapshot) {
+                if (snapshot.val() == null) {
+                    //not found
+                } else {
+                    let qidList = Object.keys(snapshot.val());
+                    let prevQueue = "";
+                    for (
+                        let i = 0;
+                        i < Object.keys(snapshot.val()).length;
+                        i++
+                    ) {
+                        let qid = qidList[i];
+                        let access = snapshot.val()[qid];
+                        queuesRef.child(qid).on("value", function (snapshot) {
+                            if (snapshot.val() == null) {
+                                //queue not found
+                            } else {
+                                let title = snapshot.val().tittle;
+                                let time = snapshot.val().time;
+                                let owner = snapshot.val().owner;
+
+                                prevQueue =
+                                    `<div class="col-md-4 col-sm-6">
 											<div class='prev-item'>
 												<h3>Title: ${title}</h3>
 												<h4 class='prev-item-id'>ID: ${qid}<h4>
 												<h4>time: ${time}<h4>
 												<h4>access: ${access}<h4>
 												<div class="prev-details">
-													<button class="prev-details-btn">see details</button>
+													<button name="seeQueueDetails" id="${qid}" class="prev-details-btn">see details</button>
 												</div>
 											</div>
 								</div>` + prevQueue;
-							}
-							prevContainer.innerHTML = prevQueue;
-						});
-					}
-				}
-			});
-	} else {
-		// No user is signed in.
-	}
+                            }
+                            prevContainer.innerHTML = prevQueue;
+                        });
+                    }
+                }
+            });
+    } else {
+        // No user is signed in.
+    }
 });
 
+const myfunction = (params, calledFrom) => {
+    prevContainer.innerHTML = "";
+    prevContainer.classList.add("d-block");
+    prevContainer.classList.remove("row");
+    const {
+        qid,
+        email,
+        owner,
+        title,
+        time,
+        inviteList,
+        counterList,
+        deskList,
+    } = params;
+    console.log("invite list ", inviteList);
+    console.log("counter list ", counterList[1]);
+    console.log("desk list", deskList);
+    let counterListLi = ``;
+    if (counterList === undefined) {
+        counterListLi = `No one is in the Counter List`;
+    } else {
+        for (const e of counterList[1]) {
+            console.log(counterListLi);
+            const li = `<li>${e}</li>`;
+            counterListLi = counterListLi + li;
+        }
+    }
+    let invitedListLi = ``;
+    if (inviteList === undefined) {
+        invitedListLi = `No one is in the Invited List`;
+    } else {
+        for (const e in inviteList) {
+            const li = `<li>${e}</li>`;
+            invitedListLi = invitedListLi + li;
+        }
+    }
 
+    prevContainer.innerHTML = `
+        <div class="row">
+            <div class="col-sm-8">
+                <h4>Queue ID: <span class="text-danger">${qid}</span></h4>
+                <h4>Queue Title: <span class="text-capitalize">${title}</span></h4>
+                <h4>Access Type: ${owner}</h4>
+            </div>
+            <div class="col-sm-4">
+                eikhane queue er QR code boshaile valo lagbo
+            </div>
+        </div>
+        <div class="row gx-3 mt-4">
+            <div class="col-sm-4">
+                <div class="p-3 bg-light rounded-3">
+                    <h4 class="text-center">Counter</h4>
+                    <ul>
+                        ${counterListLi}
+                    </ul>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="p-3 bg-light rounded-3">
+                    <h4 class="text-center">Desk</h4>
+                </div>
+            </div>
+            <div class="col-sm-4">
+                <div class="p-3 bg-light rounded-3">
+                    <h4 class="text-center">invited list</h4>
+                    <ul>
+                        ${invitedListLi}
+                    </ul>
+                </div>
+            </div>
+        </div>
+    `;
+};
+
+prevContainer.addEventListener("click", (e) => {
+    if (e.target.tagName === "BUTTON") {
+        if (e.target.name === "seeQueueDetails") {
+            seeDetails(e.target.id, "prev");
+        }
+    }
+});
--- js/queues.js
+++ js/queues.js
@@ -1,4 +1,3 @@
-
 var queuesRef = firebase.database().ref("queues");
 function createQueue(newQueue) {
     let qid = createKey();
@@ -8,8 +7,8 @@ function createQueue(newQueue) {
     let type = newQueue.queueDeskType;
     let inviteList = newQueue.invitedEmails;
     let owner = filterPath(getUserEmail());
-    let notice="";
-    let lastSL=0;
+    let notice = "";
+    let lastSL = 0;
     if (!isSignedIn()) {
         return;
     }
@@ -20,126 +19,114 @@ function createQueue(newQueue) {
 
         objInvite[email] = access;
 
-        usersRef
-            .child(email + "/inviteList")
-            .update({
-                [qid]: access
-            });
-    }
-    queuesRef.child(qid)
-        .set({
-            tittle: tittle,
-            time: time,
-            type: type,
-            owner: owner,
-            inviteList: objInvite,
-            notice: notice,
-            lastSL: lastSL
+        usersRef.child(email + "/inviteList").update({
+            [qid]: access,
         });
-    addToPrevious(owner,qid,"owner");
+    }
+    queuesRef.child(qid).set({
+        tittle: tittle,
+        time: time,
+        type: type,
+        owner: owner,
+        inviteList: objInvite,
+        notice: notice,
+        lastSL: lastSL,
+    });
+    addToPrevious(owner, qid, "owner");
 }
-function addToPrevious(email,qid,access){
-    email=filterPath(email);
-    usersRef.child(email+"/previousList").update({
-        [qid]: access
+function addToPrevious(email, qid, access) {
+    email = filterPath(email);
+    usersRef.child(email + "/previousList").update({
+        [qid]: access,
     });
 }
-function addToCounter(email,qid){
-    qIdRef=queuesRef.child(qid);
-    email= filterPath(email);
-    qIdRef.child("counterList").once("value",function(snapshot){
-        if(snapshot.val()==null){
-            qIdRef.child("counterList").update(
-                {
-                    "1":{
-                        "email":email
-                    }
-                }
-            );
-
-        }else{
-            let lastKey= Object.keys(snapshot.val())[Object.keys(snapshot.val()).length-1];
-            lastKey= parseInt(lastKey)+1;
-            qIdRef.child("counterList").update(
-                {
-                    [lastKey]: {
-                        "email": email
-                    }
+function addToCounter(email, qid) {
+    qIdRef = queuesRef.child(qid);
+    email = filterPath(email);
+    qIdRef.child("counterList").once("value", function (snapshot) {
+        if (snapshot.val() == null) {
+            qIdRef.child("counterList").update({
+                1: {
+                    email: email,
+                },
+            });
+        } else {
+            let lastKey = Object.keys(snapshot.val())[
+                Object.keys(snapshot.val()).length - 1
+            ];
+            lastKey = parseInt(lastKey) + 1;
+            qIdRef.child("counterList").update({
+                [lastKey]: {
+                    email: email,
+                },
             });
         }
-       
     });
 }
-function addToDesk(email,qid){
-    qIdRef=queuesRef.child(qid);
-    email= filterPath(email);
-    qIdRef.child("deskList").once("value",function(snapshot){
-        if(snapshot.val()==null){
-            qIdRef.child("deskList").update(
-                {
-                    "1":{
-                        "email":email
-                    }
-                }
-            );
-
-        }else{
-            let lastKey= Object.keys(snapshot.val())[Object.keys(snapshot.val()).length-1];
-            lastKey= parseInt(lastKey)+1;
-            qIdRef.child("deskList").update(
-                {
-                    [lastKey]: {
-                        "email": email
-                    }
+function addToDesk(email, qid) {
+    qIdRef = queuesRef.child(qid);
+    email = filterPath(email);
+    qIdRef.child("deskList").once("value", function (snapshot) {
+        if (snapshot.val() == null) {
+            qIdRef.child("deskList").update({
+                1: {
+                    email: email,
+                },
+            });
+        } else {
+            let lastKey = Object.keys(snapshot.val())[
+                Object.keys(snapshot.val()).length - 1
+            ];
+            lastKey = parseInt(lastKey) + 1;
+            qIdRef.child("deskList").update({
+                [lastKey]: {
+                    email: email,
+                },
             });
         }
-       
     });
 }
-function deleteFromInviteLists(email,qid){
-    email=filterPath(email);
+function deleteFromInviteLists(email, qid) {
+    email = filterPath(email);
     //remove from qid
-    queuesRef.child(qid+"/inviteList/"+email).remove();
+    queuesRef.child(qid + "/inviteList/" + email).remove();
 
     //remove from userid
-    usersRef.child(email+"/inviteList/"+qid).remove();
-
-} 
-function removeFromCounter(email,qid){
-    email=filterPath(email);
-    //remove from qid
-    queuesRef.child(qid+"/counterList/"+email).remove();
-    //remove from userid previous
-    usersRef.child(email+"/previousList/"+qid).remove();
+    usersRef.child(email + "/inviteList/" + qid).remove();
 }
-function removeFromDesk(email,qid){
-    email=filterPath(email);
+function removeFromCounter(email, qid) {
+    email = filterPath(email);
     //remove from qid
-    queuesRef.child(qid+"/deskList/"+email).remove();
+    queuesRef.child(qid + "/counterList/" + email).remove();
     //remove from userid previous
-    usersRef.child(email+"/previousList/"+qid).remove();
+    usersRef.child(email + "/previousList/" + qid).remove();
 }
-function acceptInvitation(email,qid,access){
-    if(access=="Counter"){
-        addToCounter(email,qid);
-    }
-    else if(access=="Desk"){
-        addToDesk(email,qid);
+function removeFromDesk(email, qid) {
+    email = filterPath(email);
+    //remove from qid
+    queuesRef.child(qid + "/deskList/" + email).remove();
+    //remove from userid previous
+    usersRef.child(email + "/previousList/" + qid).remove();
+}
+function acceptInvitation(email, qid, access) {
+    if (access == "Counter") {
+        addToCounter(email, qid);
+    } else if (access == "Desk") {
+        addToDesk(email, qid);
     }
     //add to previous
-    addToPrevious(email,qid,access);
+    addToPrevious(email, qid, access);
     //delete from invite.
-    deleteFromInviteLists(email,qid);
+    deleteFromInviteLists(email, qid);
 }
-function declineInvitation(email,qid){
-    deleteFromInviteLists(email,qid);
+function declineInvitation(email, qid) {
+    deleteFromInviteLists(email, qid);
 }
 function joinQueue(qid) {
     queuesRef.child(qid).on("value", function (snapshot) {
         if (snapshot.val() == null) {
             failToJoin();
-        }
-        else {
+        } else {
             let inviteList = snapshot.val().inviteList;
             let owner = snapshot.val().owner;
             let tittle = snapshot.val().tittle;
@@ -149,10 +136,10 @@ function joinQueue(qid) {
             let deskList = snapshot.val().deskList;
             let waitingList = snapshot.val().waitingList;
             let serviceList = snapshot.val().serviceList;
-            let notice=snapshot.val().notice;
+            let notice = snapshot.val().notice;
             const param = {
                 qid,
-                inviteList, 
+                inviteList,
                 owner,
                 tittle,
                 time,
@@ -161,156 +148,134 @@ function joinQueue(qid) {
                 deskList,
                 waitingList,
                 serviceList,
-                notice
+                notice,
             };
 
             // console.log(waitingList);
 
             mainUi(param);
-
         }
     });
-
 }
 function addPeople(qid, name) {
     console.log("addPeople fuction called");
     qRef = queuesRef.child(qid);
     qRef.child("lastSL").once("value", function (snapshot) {
-        if(snapshot.val()==null){
-           
-        }else{
-           
-       
-            let serialNo= snapshot.val()+1;
-            
+        if (snapshot.val() == null) {
+        } else {
+            let serialNo = snapshot.val() + 1;
+
             qidRef = queuesRef.child(qid).update({
-                lastSL: serialNo
+                lastSL: serialNo,
             });
-            queuesRef.child(qid + "/waitingList").update(
-                {
-                    [serialNo]: name
-                }
-            );
+            queuesRef.child(qid + "/waitingList").update({
+                [serialNo]: name,
+            });
         }
-        
     });
 }
-function callPeople(qid,counter) {
-    
+function callPeople(qid, counter) {
     qidRef = queuesRef.child(qid);
     qidRef.child("waitingList").once("value", function (snapshot) {
-      if(snapshot.val()==null){
+        if (snapshot.val() == null) {
+        } else {
+            let firstKey = Object.keys(snapshot.val())[0];
+            let peopleName = snapshot.val()[firstKey];
 
-      }else{
-        let firstKey=Object.keys(snapshot.val())[0];
-        let peopleName=snapshot.val()[firstKey];
-        
-        //remove person from waiting list
-        qidRef.child("waitingList/" + firstKey).remove();
+            //remove person from waiting list
+            qidRef.child("waitingList/" + firstKey).remove();
 
-        //add person to service list
-        qidRef.child("serviceList/"+firstKey).set({
-            counter: counter,
-            name: peopleName
-        });
-
-      }
+            //add person to service list
+            qidRef.child("serviceList/" + firstKey).set({
+                counter: counter,
+                name: peopleName,
+            });
+        }
     });
 }
 
-function canAddPeople(qid){
+function canAddPeople(qid) {
     qidRef = queuesRef.child(qid);
-    email=filterPath(getUserEmail());
-    qidRef.child("deskList").once("value",function(snapshot){
-        keys=Object.keys(snapshot.val());
-        flag=false;
-        for(let i=0;i<keys.length;i++){
-            if(snapshot.val()[keys[i]]['email']==email){
-                flag=true;
+    email = filterPath(getUserEmail());
+    qidRef.child("deskList").once("value", function (snapshot) {
+        keys = Object.keys(snapshot.val());
+        flag = false;
+        for (let i = 0; i < keys.length; i++) {
+            if (snapshot.val()[keys[i]]["email"] == email) {
+                flag = true;
                 break;
-            }
-            else{
-                console.log(snapshot.val()[keys[i]]['email']);
+            } else {
+                console.log(snapshot.val()[keys[i]]["email"]);
             }
         }
-            if(flag){
-                //return true
-            }
-            else{
-                //return false
-            }
+        if (flag) {
+            //return true
+        } else {
+            //return false
+        }
     });
-
 }
-function canCallPeople(qid){
+function canCallPeople(qid) {
     qidRef = queuesRef.child(qid);
-    email=filterPath(getUserEmail());
-    qidRef.child("counterList").once("value",function(snapshot){
-        keys=Object.keys(snapshot.val());
-        flag=false;
-        for(let i=0;i<keys.length;i++){
-            if(snapshot.val()[keys[i]]['email']==email){
-                flag=true;
+    email = filterPath(getUserEmail());
+    qidRef.child("counterList").once("value", function (snapshot) {
+        keys = Object.keys(snapshot.val());
+        flag = false;
+        for (let i = 0; i < keys.length; i++) {
+            if (snapshot.val()[keys[i]]["email"] == email) {
+                flag = true;
                 break;
-            }
-            else{
-                console.log(snapshot.val()[keys[i]]['email']);
+            } else {
+                console.log(snapshot.val()[keys[i]]["email"]);
             }
         }
-            if(flag){
-                //return true
-            }
-            else{
-                //return false
-            }
+        if (flag) {
+            //return true
+        } else {
+            //return false
+        }
     });
-
 }
-function canUpdateNoticeboard(qid){
+function canUpdateNoticeboard(qid) {
     qidRef = queuesRef.child(qid);
-    email=filterPath(getUserEmail());
-    qidRef.child("owner").once("value",function(snapshot){
-       if(snapshot.val()==email){
-           // return true
-       }
-       else{
-           //return false
-       }
+    email = filterPath(getUserEmail());
+    qidRef.child("owner").once("value", function (snapshot) {
+        if (snapshot.val() == email) {
+            // return true
+        } else {
+            //return false
+        }
     });
 }
-function updateNoticeBoard(qid,notice)
-{
-
+function updateNoticeBoard(qid, notice) {
     qidRef = queuesRef.child(qid).update({
-        notice: notice
+        notice: notice,
     });
-    
-    
 }
-function seeDetails(qid){
+function seeDetails(qid, calledFrom) {
     firebase.auth().onAuthStateChanged(function (user) {
         if (user) {
-            let email=filterPath(user.email);
-            queuesRef.child(qid).on("value",function(snapshot){
-                let owner=snapshot.val().owner;
-                let title=snapshot.val().tittle;
-                let time=snapshot.val().time;
-                let inviteList=snapshot.val().inviteList;
-                let counterList=snapshot.val().counterList;
-                let deskList=snapshot.val().deskList;
+            let email = filterPath(user.email);
+            queuesRef.child(qid).on("value", function (snapshot) {
+                let owner = snapshot.val().owner;
+                let title = snapshot.val().tittle;
+                let time = snapshot.val().time;
+                let inviteList = snapshot.val().inviteList;
+                let counterList = snapshot.val().counterList;
+                let deskList = snapshot.val().deskList;
 
-                let param={
+                let param = {
+                    qid,
+                    email,
                     owner,
                     title,
                     time,
                     inviteList,
                     counterList,
-                    deskList
+                    deskList,
                 };
-                return param;
+                myfunction(param, calledFrom);
             });
         }
     });
 }
-
-
